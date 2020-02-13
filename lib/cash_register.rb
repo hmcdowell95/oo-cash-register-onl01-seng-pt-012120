73ce02bab330c8b1a3310e6f36b7fad746de2627
Harley McDowell <hmcdowell95@gmail.com>
2020-02-13 02:55:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
-
+class CashRegister
+  
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @list = {}
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(i, v, q = 1)
+    a = v * q
+    @total += a
+    q.times {@items << i}
+  end
+    
+  def apply_discount
+    if @discount == 0
+      puts "There is no discount to apply."
+    else
+      a = 100.0 - @discount
+      b = a / 100.0
+      @total * b
+      puts "After the discount, the total comes to $#{total}."
+    end
+  end
+    
+  def items
+    @items
+  end
+    
+  def void_last_transaction
+    
+  end
+end
+  
