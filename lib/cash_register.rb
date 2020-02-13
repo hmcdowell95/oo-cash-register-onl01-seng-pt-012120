e2c8d9a6b53133ba165de7aef29ab7f81c08e3fb
Harley McDowell <hmcdowell95@gmail.com>
2020-02-13 03:07:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :cash_register, :cash_register_with_discount
   
-  def initialize(discount = 0)
+  def initialize(discount)
     @total = 0
     @discount = discount
     @items = []
